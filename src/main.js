82d203f1e886696d0ca3ade6aad3a45324258414
thiago <thiagoreismoura@gmail.com>
2023-09-12 17:42:23 -0300

    Ajustes

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function(){
+    const buttons = document.querySelectorAll('[data-tab-button]');
     
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(botao) {
